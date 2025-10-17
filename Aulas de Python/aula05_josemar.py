44c64ae75701cd6815f34043fc410444635890db
joaobender788 <joaogabrielbmartins@gmail.com>
2025-10-17 11:24:54 -0300

    Create aula05_josemar.py

---
+++ Aulas de Python/aula05_josemar.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+'''alunos = pd.read_csv("alunos.csv")
+notas = pd.read_csv("notas.csv")
+
+dados_completos = pd.merge(alunos, notas, on="Matricula")
+
+print("=== Dados combinados ===")
+print(dados_completos)'''
+
+# Concatenação
+'''dados1 = pd.read_csv("dados1.csv")
+dados2 = pd.read_csv("dados2.csv")
+
+todos_dados = pd.concat([dados1, dados2])
+
+print(todos_dados)'''
