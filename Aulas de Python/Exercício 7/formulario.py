25714e76c65b27a9f7d4d1073fa14da0ce2f215d
joaobender788 <joaogabrielbmartins@gmail.com>
2025-10-29 11:08:44 -0300

    Create formulario.py

---
+++ Aulas de Python/Exercício 7/formulario.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("form.html")
+@app.route("/resultado", methods=["POST"])
+def resultado():
+    nome = request.form["nome"]
+    idade = request.form["idade"]
+    cidade = request.form["cidade"]
+    return f"Olá, {nome}! você tem {idade} anos e mora em {cidade}."
+if __name__ == "__main__":
+    app.run(debug=True)
