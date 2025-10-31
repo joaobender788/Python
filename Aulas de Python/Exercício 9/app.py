a5b3d95b6534ad36d52b9bedd9e96039d08e3fd7
joaobender788 <joaogabrielbmartins@gmail.com>
2025-10-31 10:01:41 -0300

    Add files via upload

--- Aulas de Python/Exercício 9/app.py
+++ Aulas de Python/Exercício 9/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+@app.route('/resultado', methods=['POST'])
+def resultado():
+    nome = request.form['nome']
+    idade = request.form['idade']
+    cidade = request.form['cidade']
+    email = request.form['email']
+    telefone = request.form['telefone']
+    return f"<h2>Olá {nome}, você tem {idade} anos e mora em {cidade}! Seu e-mail é {email} e seu telefone é {telefone}.</h2>"
+if __name__ == '__main__':
+    app.run(debug=True)
